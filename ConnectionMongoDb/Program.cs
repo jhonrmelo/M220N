8c2f694eb4942ec4add2fcd3554a8a83a421d262
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2022-04-10 21:25:02 -0300

    adding project

---
+++ ConnectionMongoDb/Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+using System;
+
+namespace ConnectionMongoDb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb+srv://rideit:k2GWIZywBklhOguH@ride-it.o4pze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+            var db = client.GetDatabase("sample_mflix");
+
+            var collection = db.GetCollection<BsonDocument>("movies");
+
+            var result = collection.Find(new BsonDocument()).SortByDescending(m => m["runtime"]).Limit(10).ToList();
+
+            foreach (var movie in result)
+            {
+                Console.WriteLine(movie.GetValue("title"));
+            }
+        }
+    }
+}
---
+++ M220N/Repositories/CommentsRepository.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using M220N.Models;
+using M220N.Models.Projections;
+using M220N.Models.Responses;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace M220N.Repositories
+{
+    public class CommentsRepository
+    {
+        private readonly IMongoCollection<Comment> _commentsCollection;
+        private readonly MoviesRepository _moviesRepository;
+
+        public CommentsRepository(IMongoClient mongoClient)
+        {
+            var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+            _commentsCollection = mongoClient.GetDatabase("sample_mflix").GetCollection<Comment>("comments");
+            _moviesRepository = new MoviesRepository(mongoClient);
+        }
+
+        /// <summary>
+        ///     Adds a comment.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="movieId"></param>
+        /// <param name="comment"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>The Movie associated with the comment.</returns>
+        public async Task<Movie> AddCommentAsync(User user, ObjectId movieId, string comment,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var newComment = new Comment
+                {
+                    Date = DateTime.UtcNow,
+                    Text = comment,
+                    Name = user.Name,
+                    Email = user.Email,
+                    MovieId = movieId
+                };
+
+                // Ticket: Add a new Comment
+                // Implement InsertOneAsync() to insert a
+                // new comment into the comments collection.
+                await _commentsCollection.InsertOneAsync(newComment);
+
+                return await _moviesRepository.GetMovieAsync(movieId.ToString(), cancellationToken);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///     Updates an existing comment. Only the comment owner can update the comment.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="movieId"></param>
+        /// <param name="commentId"></param>
+        /// <param name="comment"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>An UpdateResult</returns>
+        public async Task<UpdateResult> UpdateCommentAsync(User user,
+            ObjectId movieId, ObjectId commentId, string comment,
+            CancellationToken cancellationToken = default)
+        {
+            return await _commentsCollection.UpdateOneAsync(
+            Builders<Comment>.Filter.Where(x => x.Email == user.Email && x.Id == commentId),
+            Builders<Comment>.Update.Set(x => x.Text, comment),
+            new UpdateOptions { IsUpsert = false },
+            cancellationToken);
+        }
+
+        /// <summary>
+        ///     Deletes a comment. Only the comment owner can delete a comment.
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <param name="commentId"></param>
+        /// <param name="user"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>The movie associated with the comment that is being deleted.</returns>
+        public async Task<Movie> DeleteCommentAsync(ObjectId movieId, ObjectId commentId,
+            User user, CancellationToken cancellationToken = default)
+        {
+            // Ticket: Delete a Comment
+            // Implement DeleteOne() to delete an
+            // existing comment. Remember that only the original
+            // comment owner can delete the comment!
+            _commentsCollection.DeleteOne(
+                Builders<Comment>.Filter.Where(c => c.Id == commentId && c.Email == user.Email));
+
+            return await _moviesRepository.GetMovieAsync(movieId.ToString(), cancellationToken);
+        }
+
+        public async Task<TopCommentsProjection> MostActiveCommentersAsync()
+        {
+            try
+            {
+                List<ReportProjection> result = null;
+
+                result = await _commentsCollection
+                  .WithReadConcern(ReadConcern.Majority)
+                  .Aggregate()
+                  .Group(x => x.Email, y => new ReportProjection()
+                  {
+                      Id = y.Key,
+                      Count = y.Count()
+                  })
+                  .Sort(Builders<ReportProjection>.Sort.Descending(x => x.Count))
+                  .Limit(20)
+                  .ToListAsync();
+
+                return new TopCommentsProjection(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
